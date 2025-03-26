112517f19f1fe6a8d7235847c3bab9a2dfa3fa7c
Nicolas Rodriguez <nicolasrdz.05@gmail.com>
2025-03-26 23:50:52 +0000

    Agrego evaluacion.js

---
+++ evaluacion.js
@@ -0,0 +1,10 @@
+function  convertirEdad (edad)  { 
+
+    if ( edad  >=  18 )  { 
+        console.log ( `Eres mayor de edad. ${edad}`) 
+    } else {
+        console.log( `Eres menor de edad. ${edad}`) 
+    }
+   
+}
+convertirEdad (20)
